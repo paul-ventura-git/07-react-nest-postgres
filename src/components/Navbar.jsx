f9b3a8a037fcd42e9b6246f2e61260df8847476c
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-28 16:26:56 -0500

    Crud User

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,41 @@
 import React from 'react'
-import { Container } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
 
-    <nav class="navbar bg-dark navbar-expand-lg bg-body-tertiary mb-5" data-bs-theme="dark">
-      <div class="container">
-        <a class="navbar-brand" href="#">Ventura Netzwerk</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+    <nav className="navbar bg-dark navbar-expand-lg bg-body-tertiary mb-5" data-bs-theme="dark">
+      <div className="container">
+        <a className="navbar-brand" href="#">Ventura Netzwerk</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <Link class="nav-link" aria-current="page" to="/home">Home</Link>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/home">Home</Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/gallery">Gallery</Link>
+            <li className="nav-item">
+              <Link className="nav-link" to="/gallery">Gallery</Link>
             </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Management
               </a>
-              <ul class="dropdown-menu">
-                <li><Link class="dropdown-item" to="/cruduser">Users</Link></li>
-                <li><Link class="dropdown-item" to="/crudproduct">Products</Link></li>
-                <li><hr class="dropdown-divider" /></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <ul className="dropdown-menu">
+                <li><Link className="dropdown-item" to="/cruduser">Users</Link></li>
+                <li><Link className="dropdown-item" to="/crudproduct">Products</Link></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
               </ul>
             </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            <li className="nav-item">
+              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
             </li>
           </ul>
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button class="btn btn-outline-success" type="submit">Search</button>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
           </form>
         </div>
       </div>
--- src/pages/CrudUser.jsx
+++ src/pages/CrudUser.jsx
@@ -1,38 +1,72 @@
 import React from 'react'
+import { useEffect, useState } from "react"
 
 function CrudUser() {
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+
+    let my_key = process.env.REACT_APP_ACCESS_TOKEN;
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", "Bearer "+my_key);
+    
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow"
+    };
+
+    fetch("http://localhost:3000/user", requestOptions)
+      .then(response => response.json())
+      .then(json => setUsers(json))
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
   return (
-    <div className='container'>
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-          </tr>
-        </thead>
-        <tbody class="table-group-divider">
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <div className="CrudUser">
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+      
+      <div className='container'>
+
+        <h1>Users</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Username</th>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+              <th scope="col">E-mail</th>
+              <th scope="col">Is Active</th>
+            </tr>
+          </thead>
+
+          <tbody className="table-group-divider">
+
+            {users.map(user => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.username}</td>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.email}</td>
+                <td>{user.isActive ? "Active" : "No active"}</td>
+              </tr>
+            ))}
+
+          </tbody>
+        </table>
+
+      </div>
+
+    )}
+  </div>
   )
 }
 
