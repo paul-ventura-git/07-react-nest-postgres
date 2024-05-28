07c8b407d8560eb115f50b4271de6ec31468b128
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-28 15:18:59 -0500

    Login, corrected

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,35 +1,66 @@
 import React from 'react'
+import { useState } from 'react'
 import '../styles/login.css'
 import image from '../img/bootstrap-logo.svg'
 
 function Login() {
+
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(inputs.password + inputs.username);
+  }
+  console.log(inputs);
   return (
-    <div className="container text-center py-5">
-      <main className="form-signin w-100 m-auto">
-        <form>
-          <img className="mb-4" src={image} alt="" width="72" height="57"/>
-          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-          
-          <div className="form-floating">
-            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-            <label htmlFor="floatingInput">Email address</label>
-          </div>
-          <br></br>
-          <div className="form-floating">
-            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-            <label htmlFor="floatingPassword">Password</label>
-          </div>
-          <br></br>
-          <div className="checkbox mb-3">
-            <label>
-              <input type="checkbox" value="remember-me" /> Remember me
-            </label>
-          </div>
-          <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-          <p className="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
-        </form>
-      </main>
-    </div>
+    
+      <div className="container text-center py-5">
+        <main className="form-signin w-100 m-auto">
+          <form onSubmit={handleSubmit}>
+            <img className="mb-4" src={image} alt="" width="72" height="57"/>
+            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+            
+            <div className="form-floating">
+              <input 
+                name="username"
+                type="email" 
+                className="form-control" 
+                id="floatingInput" 
+                placeholder="name@example.com" 
+                onChange={handleChange}
+              />
+              <label htmlFor="floatingInput">Username or Email address</label>
+            </div>
+            <br></br>
+            <div className="form-floating">
+              <input 
+                name="password"
+                type="password" 
+                className="form-control" 
+                id="floatingPassword" 
+                placeholder="Password"
+                onChange={handleChange}
+              />
+              <label htmlFor="floatingPassword">Password</label>
+            </div>
+            <br></br>
+            <div className="checkbox mb-3">
+              <label>
+                <input type="checkbox" value="remember-me" /> Remember me
+              </label>
+            </div>
+            <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+            <p className="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+          </form>
+        </main>
+      </div>
+    
   )
 }
 
