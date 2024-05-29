a50393111d0cff3ede25c1ea148249b99e9d83ee
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-29 13:08:43 -0500

    Protected routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, Navigate, Outlet } from "react-router-dom";
 
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
@@ -9,7 +9,28 @@ import CrudProduct from "./pages/CrudProduct";
 import NoPage from "./pages/NoPage";
 import Login from "./pages/Login";
 
+const ProtectedRoute = ({
+  isAllowed,
+  redirectPath = '/home',
+  children,
+}) => {
+  if (!isAllowed) {
+    return <Navigate to={redirectPath} replace />;
+  }
+
+  return children ? children : <Outlet />;
+};
+
 function App() {
+  let user1 = {
+    user: "paul",
+    permissions: ["editor", "admin"]
+  };
+  let user = {
+    user: "michael",
+    permissions: ["customer", "premium"]
+  };
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -17,11 +38,32 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="home" element={<Home />} />
-            <Route path="gallery" element={<Gallery />} />
-            <Route path="productview" element={<ProductView />} />
-            <Route path="cruduser" element={<CrudUser />} />
-            <Route path="crudproduct" element={<CrudProduct />} />
-            
+            <Route element={<ProtectedRoute isAllowed={!!user} />}>
+              <Route path="gallery" element={<Gallery />} />
+              <Route path="productview" element={<ProductView />} />
+            </Route>
+            <Route 
+              path="cruduser" 
+              element={
+                <ProtectedRoute
+                  redirectPath="/home"
+                  isAllowed={!!user && user.permissions.includes('editor')}
+                >
+                  <CrudUser />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path="crudproduct" 
+              element={
+                <ProtectedRoute
+                  redirectPath="/home"
+                  isAllowed={!!user && user.permissions.includes('editor')}
+                >
+                  <CrudProduct />
+                </ProtectedRoute>
+              } 
+            />            
             <Route path="*" element={<NoPage />} />
           </Route>
           <Route path="login" element={<Login />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { useState } from 'react';
 
 function Navbar() {
+
+  const [user, setUser] = useState([]);
+
+  const handleLogin = () =>
+    setUser({
+      id: '1',
+      name: 'Robin',
+      permissions: ['editor'],
+      roles: ['admin'],
+    });
+
+  const handleLogout = () => setUser(null);
+  console.log(user.name);
   return (
 
     <nav className="navbar bg-dark navbar-expand-lg bg-body-tertiary mb-5" data-bs-theme="dark">
       <div className="container">
-        <a className="navbar-brand" href="#">Ventura Netzwerk</a>
+        <a className="navbar-brand" href="/">Ventura Netzwerk</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -19,24 +33,36 @@ function Navbar() {
               <Link className="nav-link" to="/gallery">Gallery</Link>
             </li>
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a className="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Management
               </a>
               <ul className="dropdown-menu">
                 <li><Link className="dropdown-item" to="/cruduser">Users</Link></li>
                 <li><Link className="dropdown-item" to="/crudproduct">Products</Link></li>
                 <li><hr className="dropdown-divider" /></li>
-                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                <li><a className="dropdown-item" href="/">Something else here</a></li>
               </ul>
             </li>
             <li className="nav-item">
               <a className="nav-link disabled" aria-disabled="true">Disabled</a>
             </li>
           </ul>
-          <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
-          </form>
+
+          { (user==null || user===undefined) ? (
+            <>
+              <form className="d-flex" role="search">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success" type="submit">Search</button>
+              </form>
+              <button className="btn btn-success" onClick={handleLogin} style={{marginLeft: "10px"}}>Sign In</button>
+            </>            
+          ) : (
+            <>
+              <div className='nav-item'>Welcome, {user.name}</div>
+              <button className="btn btn-success" onClick={handleLogout} style={{marginLeft: "10px"}}>Sign Out</button>
+            </>
+            
+          )}
         </div>
       </div>
     </nav>
